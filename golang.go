d1674a6db85594c5290d7e4dfe031dd5a64deb0f
Kacper Marczewski <kacper.marczewski@gmail.com>
2024-02-19 16:18:31 +0100

    primes

--- golang.go
+++ golang.go
@@ -1,5 +1,35 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
 func main() {
-    fmt.Println("hello world")
+	const FROM int32 = 10000000
+	const TO int32 = 10001000
+	var limit int32 = int32(math.Sqrt(float64(TO)))
+	var a [TO - FROM]int8
+
+	var start = time.Now().UnixMilli()
+
+	for n := FROM; n < TO; n++ {
+		for i := 2; i < int(limit); i++ {
+			if int(n)%i == 0 {
+				a[n-FROM] = 1
+				break
+			}
+		}
+	}
+
+	var elapsed = time.Now().UnixMilli() - start
+
+	for n := FROM; n < TO; n++ {
+		if a[n-FROM] == 0 {
+			fmt.Println(n)
+		}
+	}
+
+	fmt.Println("time = ", elapsed)
 }
